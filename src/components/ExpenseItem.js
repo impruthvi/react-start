49d41bafdf9544722beb257bff949cdbc32f2622
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-05-16 22:42:30 +0530

    adding basic css styoe in ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
-    return <h2>asdasd</h2>;
+    return (
+        <div className="expense-item">
+            <div>13 May 2001</div>
+            <div className="expense-item__description">
+
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$200</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
