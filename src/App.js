33c94b4fe8b5a5297ec18710a8c9d59b8ec1c3ea
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-05-17 16:08:35 +0530

    added a dynamic data using props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import ExpenseItem from './components/ExpenseItem'
 function App() {
+  const expense = [
+    {id:1,title: 'Test 1',amount:100,date: new Date(2000,1,1)},
+    {id:2,title: 'Test 2',amount:123,date: new Date(123,1,1)},
+
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
+      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
     </div>
   );
 }
