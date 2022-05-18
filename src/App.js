6937e3af6064e0f78f536c8dabf467c5a6a800dd
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-05-18 16:13:22 +0530

    separate Card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import ExpenseItem from './components/ExpenseItem'
+import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 function App() {
   const expense = [
     {id:1,title: 'Test 1',amount:100,date: new Date(2000,1,1)},
-    {id:2,title: 'Test 2',amount:123,date: new Date(123,1,1)},
+    {id:2,title: 'Test 2',amount:123,date: new Date(2001,1,1)},
 
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
-      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
+        <Expenses items={expense}/>
     </div>
   );
 }
