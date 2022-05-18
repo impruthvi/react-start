a1179b8d5e713f3baee5435a9a0b4e194d0c2171
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-05-18 15:22:59 +0530

    separate the Expense Date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
    
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+           
             <div className="expense-item__description">
-
+                <ExpenseDate date={props.date}/>
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
