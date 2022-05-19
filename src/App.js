26149f704d2142df4eef923ea78724e76d4b2028
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-05-19 14:40:15 +0530

    apply an arrow function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ExpenseItem from './components/Expense/ExpenseItem';
 import Expenses from './components/Expense/Expenses';
-function App() {
+const  App = () =>{
   const expense = [
     {id:1,title: 'Test 1',amount:100,date: new Date(2000,1,1)},
     {id:2,title: 'Test 2',amount:123,date: new Date(2001,1,1)},
