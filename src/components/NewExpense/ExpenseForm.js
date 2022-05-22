72aa288cfa68f52ccf2dd3378f8a37092bd6d7c9
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-05-22 13:40:26 +0530

    one state insted

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,39 @@
 import React,{useState} from "react";
 import './ExpenseForm.css'
 const ExpenseForm = () => {
-    // State for title
-    const [enteredTitle,setEnteredTitle] = useState('');
+    
+    // const [enteredTitle,setEnteredTitle] = useState('');
+    // const [enteredAmount,setEnteredAmount] = useState('');
+    // const [enteredDate,setEnteredDate] = useState('');
+
+    const [userInput,setUserInput] = useState({
+        enteredTitle:'',
+        emtedAmount:'',
+        enteredDate:''
+    });
+
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
+        // setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle:event.target.value
+        });
     };
 
-    // State for Amount
-    const [enteredAmount,setEnteredAmount] = useState('');
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
+        // setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            emtedAmount:event.target.value
+        });
     }
 
-    // State for Date
-    const [enteredDate,setEnteredDate] = useState('');
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
+        // setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate:event.target.value
+        });
     }
 
     return <form>
