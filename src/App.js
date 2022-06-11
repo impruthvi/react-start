c71d3c2138c017c0bfadf5f9e6f9f6954ebf86a6
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-06-11 23:17:17 +0530

    Rendering Lists of Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from "react";
-import Expenses from './components/Expense/Expenses';
-import NewExpense from "./components/NewExpense/NewExpense"
-const  App = () =>{
-
-
-  const expense = [
-    {id:1,title: 'Test 1',amount:100,date: new Date(2000,1,1)},
-    {id:2,title: 'Test 2',amount:123,date: new Date(2001,1,1)},
-
+import React, { useState } from "react";
+import Expenses from "./components/Expense/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+const App = () => {
+  const DUMMY_EXPENSES = [
+    { id: 1, title: "Test 1", amount: 100, date: new Date(2000, 1, 1) },
+    { id: 2, title: "Test 2", amount: 123, date: new Date(2001, 1, 1) },
   ];
-  const addExpenseHandler = expense => {
-    console.log("in App");
-    console.log(expense);
-    
-  }
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div>
-        <NewExpense onAddExpense={addExpenseHandler}/>
-        <Expenses items={expense}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
