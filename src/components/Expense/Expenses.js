9d6da5410c93a5f9cf765de2aa0e7568ccb0521c
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-06-12 15:12:10 +0530

    compoted the chart functionality

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart"
 import Card from "../UI/Card";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -21,7 +22,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-
+    <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList items = {filteredExpenses}/>
     </Card>
   );
